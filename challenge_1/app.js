4bf41573e13fa7139e73dc38ba0d1a8ac156952c
Michael wu <mwuf15@gmail.com>
2020-04-20 12:05:45 -0700

    added function to alternate between x and o

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,12 @@
 
-
+  var isDefault = true;
+var addClick = function (clickId) {
+  if(isDefault){
+    document.getElementById(clickId).innerHTML = 'X';
+    isDefault = false;
+  } else {
+    document.getElementById(clickId).innerHTML = 'O';
+    isDefault = true;
+  }
+      console.log('clicked')
+}
