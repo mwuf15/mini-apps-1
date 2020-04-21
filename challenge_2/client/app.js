b37a61b33a7daf8b7de102ab4508e5ce184024a2
Michael wu <mwuf15@gmail.com>
2020-04-21 12:15:50 -0700

    got the form to post to a endpoint

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,6 @@
 
 
+//textarea form
+{/* <label for="csv"> Enter Data Here:</label>
+<textarea id="csv" name="csv"
+    rows="5" cols="33"></textarea> */}
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,20 @@
 
+const express = require('express');
+const path = require('path');
+const app = express()
+const PORT = 3000
 
+
+// app.get('/', (req, res) => {
+//   res.send('Hello Human')
+// })
+app.use(express.static('client'))
+// console.log(__dirname)
+app.post('/upload_json', (req,res) => {
+  console.log()
+  res.end('data')
+})
+
+app.listen(PORT, () => {
+  console.log(`connected and running on port ${PORT}`)
+})
