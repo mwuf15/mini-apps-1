ecb305ba37a9d79171adeee25ee10b626ee67f4b
Michael wu <mwuf15@gmail.com>
2020-04-22 14:47:33 -0700

    setup file and submit button

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,24 @@
 
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    // console.log(props)
+    this.state = {
+      formNumber: ''
+    };
+  }
+checkout() {
+  console.log('clicked')
+}
+
+render() {
+  return (
+    <div>
+      <button onClick={this.checkout}> Checkout</button>
+    </div>
+  )
+}
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,19 @@
+const express = require('express')
+const path = require('path')
+const bodyparsesr = require('body-parser')
+const app = express()
+const PORT = 4000;
 
+// app.get('/', (req, res) => {
+//   res.send('request recieved')
+// });
 
+app.post('/', (req,res) =>{
+  res.send('request posted')
+});
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log(`Running and listening on Port: ${PORT}`)
+});
