e3856712ce3749e5eb36c460af9ef4ac431e8fa0
Michael wu <mwuf15@gmail.com>
2020-07-27 15:11:17 -0700

    implemented  winner rules in isWinner

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -14,6 +14,7 @@ var currPlayer = 'X';
 // console.log ('game:',a1,a2,a3)
 // console.log('current player:', currPlayer);
 var board = {a1:'',a2:'',a3:'',b1:'',b2:'',b3:'',c1:'',c2:'',c3:''};
+var plays = 0;
 console.log(board);
 var game = function () {
   clickHandler()
@@ -41,12 +42,14 @@ var clickHandler = function () {
       board[event.target.id] = document.getElementById(event.target.id).innerHTML;
       console.log(board);
       currPlayer = 'O';
+      plays++;
       isWinner();
     } else if (currPlayer === 'O' && playerPos !== 'X') {
       document.getElementById(event.target.id).innerHTML = 'O';
       board[event.target.id] = document.getElementById(event.target.id).innerHTML;
       console.log(board);
       currPlayer = 'X'
+      plays++;
       isWinner();
 
     }
@@ -59,8 +62,40 @@ var clickHandler = function () {
 }
 
 var isWinner = function() {
-  if (board['a1'] === 'X' && board['a2'] === 'X' && board['a3'] === 'X') {
+  if (board['a1'] === 'X' && board['a2'] === 'X' && board['a3'] === 'X' && plays <=9) {
     console.log('winner')
+  } else if (board['a1'] === 'O' && board['a2'] === 'O' && board['a3'] === 'O' && plays <=9) {
+    console.log('winner')
+  } else if (board['b1'] === 'X' && board['b2'] === 'X' && board['b3'] === 'X' && plays <=9) {
+    console.log('winner')
+  } else if (board['b1'] === 'O' && board['b2'] === 'O' && board['b3'] === 'O' && plays <=9) {
+    console.log('winner')
+  } else if (board['c1'] === 'X' && board['c2'] === 'X' && board['c3'] === 'X' && plays <=9) {
+    console.log('winner')
+  } else if (board['c1'] === 'O' && board['c2'] === 'O' && board['c3'] === 'O' && plays <=9) {
+    console.log('winner')
+  } else if (board['a1'] === 'X' && board['b1'] === 'X' && board['c1'] === 'X' && plays <=9) {
+    console.log('winner')
+  } else if (board['a1'] === 'O' && board['b1'] === 'O' && board['c1'] === 'O' && plays <=9) {
+    console.log('winner')
+  } else if (board['a2'] === 'X' && board['b2'] === 'X' && board['c2'] === 'X' && plays <=9) {
+    console.log('winner')
+  } else if (board['a2'] === 'O' && board['b2'] === 'O' && board['c2'] === 'O' && plays <=9) {
+    console.log('winner')
+  } else if (board['a3'] === 'X' && board['b3'] === 'X' && board['c3'] === 'X' && plays <=9) {
+    console.log('winner')
+  } else if (board['a3'] === 'O' && board['b3'] === 'O' && board['c3'] === 'O' && plays <=9) {
+    console.log('winner')
+  } else if (board['a1'] === 'X' && board['b2'] === 'X' && board['c3'] === 'X' && plays <=9) {
+    console.log('winner')
+  } else if (board['a1'] === 'O' && board['b2'] === 'O' && board['c3'] === 'O' && plays <=9) {
+    console.log('winner')
+  } else if (board['a3'] === 'X' && board['b2'] === 'X' && board['c1'] === 'X' && plays <=9) {
+    console.log('winner')
+  } else if (board['a3'] === 'O' && board['b2'] === 'O' && board['c1'] === 'O' && plays <=9) {
+    console.log('winner')
+  } else if (plays>= 9){
+    console.log('its a tie');
   }
 
 }
