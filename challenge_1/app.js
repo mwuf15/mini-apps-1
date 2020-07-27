2915741173ff9cdb6f7ff622da10e800431b66af
Michael wu <mwuf15@gmail.com>
2020-07-27 12:35:00 -0700

    added X to board when clicked

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,16 @@
+console.log('success')
 
 
+var getId = function() {
+  const ele = document.getElementById("board")
+  ele.addEventListener("click", (event) => {
+    console.log(event.target.id);
+    // return (event.target.id)
+    document.getElementById(event.target.id).innerHTML ='X'
+  });
+}
+// console.log(getId())
+// var gameValue = document.getElementById(getId())
+getId();
+
+// gameValue.innerHTML = "X"
